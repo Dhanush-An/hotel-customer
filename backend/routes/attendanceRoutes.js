cef2959ef4e6d54b48b095f9ff7bb14a2aad764a
Dhanush-An <dhanush.antigraviity@gmail.com>
2026-03-30 12:07:06 +0530

    Standalone Final Fix: Restored root package.json and verified standalone database connection

--- backend/routes/attendanceRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAttendance, createAttendance, updateAttendance } = require('../controllers/attendanceController');
-
-router.get('/', getAttendance);
-router.post('/', createAttendance);
-router.put('/:id', updateAttendance);
-
-module.exports = router;
